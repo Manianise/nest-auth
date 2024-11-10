0fed5285a76bb43c9202a9def3b416e44adff52f
Manianise <strerbik.pierrealexandre@gmail.com>
2024-11-10 18:28:26 +0100

    10/11/2024

---
+++ src/configs/jwt-secrets.ts
@@ -0,0 +1,3 @@
+// This file is for development purposes only. Do not store sensitive data in source code
+
+    export const JWT_TOKEN= "I-will-make-it-someday";
---
+++ update.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+msg=10/11/2024
+
+git add . && git commit -m $msg && git push;
+
+
